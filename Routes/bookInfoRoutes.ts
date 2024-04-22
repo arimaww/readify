155c855fbeed6ebcd9c3615f88ba9f9c89f825f8
Islam <islammirzabekov228@gmail.com>
2024-04-22 14:48:38 +0300

    the getAllRatings has been added

--- Routes/bookInfoRoutes.ts
+++ Routes/bookInfoRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getAllCommentsForBook, getBookRatingsById, leaveComment, removeComment, removeRating, setRating } from "../Controllers/bookInfoControllers";
+import { getAllCommentsForBook, getAllRatings, getBookRatingsById, leaveComment, removeComment, removeRating, setRating } from "../Controllers/bookInfoControllers";
 
 const router = Router();
 
 router.post('/getratings', getBookRatingsById);
+router.get('/getAllRatings', getAllRatings);
 router.post('/setrating', setRating);
 router.delete('/removeRating', removeRating);
 router.post('/leaveComment', leaveComment)
