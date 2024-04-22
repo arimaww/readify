c4937e0b1eacd9aff5184d1f389a5d0bb2042816
Islam <islammirzabekov228@gmail.com>
2024-04-22 11:30:07 +0300

    rtk apis and slices for comments

--- client/src/app/services/bookInfo.ts
+++ client/src/app/services/bookInfo.ts
@@ -1,34 +1,56 @@
 import { api } from "./api";
-import { Rating } from "@prisma/client";
+import { Comments, Rating } from "@prisma/client";
 
 export type ResponseBookRatingsData = Rating
+export type ResponseCommentsData = Comments
 
 export const bookInfoApi = api.injectEndpoints({
     endpoints: (builder) => ({
-        getBookRatingsById: builder.query<ResponseBookRatingsData[], {bookId: number}>({
+        getBookRatingsById: builder.query<ResponseBookRatingsData[], { bookId: number }>({
             query: (data) => ({
                 url: "/bookInfo/getratings",
                 body: data,
                 method: "POST"
             })
         }),
-        setRating: builder.mutation<ResponseBookRatingsData, {userId: number, bookId: number, value: number}>({
+        setRating: builder.mutation<ResponseBookRatingsData, { userId: number, bookId: number, value: number }>({
             query: (data) => ({
                 url: "/bookInfo/setRating",
                 body: data,
                 method: "POST",
             })
         }),
-        removeRating: builder.mutation<ResponseBookRatingsData, {bookId: number, userId: number}>({
+        removeRating: builder.mutation<ResponseBookRatingsData, { bookId: number, userId: number }>({
             query: (data) => ({
                 url: "/bookInfo/removeRating",
                 body: data,
                 method: "DELETE"
             })
+        }),
+        leaveComment: builder.mutation<ResponseCommentsData, { bookId: number, userId: number, text: string }>({
+            query: data => ({
+                url: "/bookInfo/leaveComment",
+                body: data,
+                method: "POST"
+            })
+        }),
+        removeComment: builder.mutation<string, { commentId: number }>({
+            query: data => ({
+                url: "/bookInfo/removeComment",
+                body: data.commentId,
+                method: "DELETE"
+            })
+        }),
+        getAllCommentsForBook: builder.query<ResponseCommentsData[], { bookId: number }>({
+            query: data => ({
+                url: "/bookInfo/getAllCommentsForBook",
+                body: data.bookId,
+                method: "POST"
+            })
         })
     })
 })
 
-export const {endpoints: {getBookRatingsById, setRating, removeRating}} = bookInfoApi;
+export const { endpoints: { getBookRatingsById, setRating, removeRating, getAllCommentsForBook, leaveComment, removeComment } } = bookInfoApi;
 
-export const {useGetBookRatingsByIdQuery, useSetRatingMutation, useRemoveRatingMutation} = bookInfoApi
+export const { useGetBookRatingsByIdQuery, useSetRatingMutation, useRemoveRatingMutation, useGetAllCommentsForBookQuery, useLeaveCommentMutation, useRemoveCommentMutation } = bookInfoApi
--- client/src/features/bookInfo/bookInfoSlice.ts
+++ client/src/features/bookInfo/bookInfoSlice.ts
@@ -1,14 +1,16 @@
-import { Rating } from "@prisma/client";
+import { Comments, Rating } from "@prisma/client";
 import { createSlice } from "@reduxjs/toolkit";
 import { bookInfoApi } from "../../app/services/bookInfo";
 
 
 type InitialState = {
-    rating: Rating[] | Rating | null
+    rating: Rating[] | Rating | null,
+    comment: Comments[] | Comments | null,
 }
 
 const initialState:InitialState = {
-    rating: null
+    rating: null,
+    comment: null
 }
 
 export const bookInfoSlice = createSlice({
@@ -20,6 +22,10 @@ export const bookInfoSlice = createSlice({
             state.rating = action.payload;
         }).addMatcher(bookInfoApi.endpoints.setRating.matchFulfilled, (state, action) => {
             state.rating = action.payload;
+        }).addMatcher(bookInfoApi.endpoints.getAllCommentsForBook.matchFulfilled, (state, action) => {
+            state.comment = action.payload;
+        }).addMatcher(bookInfoApi.endpoints.leaveComment.matchFulfilled, (state, action) => {
+            state.comment = action.payload;
         })
     }
 })
