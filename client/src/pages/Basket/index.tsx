25cef33cf35833b679cae9388d8538fb03fa661b
Islam <islammirzabekov228@gmail.com>
2024-04-25 11:10:49 +0300

    added handlePurchaseBook and change BasketBuy showing

--- client/src/pages/Basket/index.tsx
+++ client/src/pages/Basket/index.tsx
@@ -6,8 +6,20 @@ import { useGetBookListQuery } from "../../app/services/book";
 import { useGetUserFavoritesQuery } from "../../app/services/auth";
 import BasketBuy from "../../components/BasketBuy";
 import styles from './Basket.module.scss'
+import { useState } from "react";
+import { useCreatePurchaseMutation } from "../../app/services/purchase";
+import { Purchase } from "@prisma/client";
+import { isErrorWithMessage } from "../../utils/is-error-with-message";
+import { useSnackbar } from "notistack";
+import { LoadingPage } from "../../components/LoadingPage";
+import { useNavigate } from "react-router-dom";
 
 export const Basket = () => {
+  const [purchaseBook, resultPurchaseBook] = useCreatePurchaseMutation();
+  const [error, setError] = useState<string>("");
+  const { enqueueSnackbar } = useSnackbar();
+  const navigate = useNavigate();
+  
   const basket = useGetUserBasketQuery();
   const user = useSelector(selectUser)
   const book = useGetBookListQuery({ id: user?.userId })
@@ -15,15 +27,40 @@ export const Basket = () => {
   const favorites = useGetUserFavoritesQuery({ userId: user?.userId });
   const entryPrice = basketList?.reduce((val, acc) => val += acc.cost, 0);
 
+  
+
+  const handlePurchaseBook = async (data: Omit<Purchase, "purchasedId">[]) => {
+    try {
+      data.map(async el => await purchaseBook(el).unwrap())
+      enqueueSnackbar("Покупка успешно совершена", { variant: "success" })
+
+      navigate('/mypurchases');
+    }
+    catch (err) {
+      if (isErrorWithMessage(err)) {
+        setError(err.data.message);
+      }
+      else {
+        setError("Произошла ошибка, повторите попытку позже")
+      }
+      enqueueSnackbar(error, { variant: "error" })
+    }
+  }
+
+  if(!basketList) return <LoadingPage />
+
+  if(resultPurchaseBook.isLoading) return <LoadingPage />
+
   return (
+
     <div className={styles.basket}>
       <h2>Моя корзина</h2>
       <div className={styles.basket__basketBookList}>
         <BasketBookList basketList={basketList} favorites={favorites.data} />
       </div>
-      <div>
-        <BasketBuy countOfBooks={basketList?.length!} fullPrice={entryPrice!} />
-      </div>
+      {basketList?.length > 0 ? <div>
+        <BasketBuy countOfBooks={basketList?.length!} fullPrice={entryPrice!} clickPurchase={handlePurchaseBook} basketList={basketList} />
+      </div> : <></>}
     </div>
   )
 }
