be73a4aa260a53b25c76623dc4fec959209eb224
Islam <islammirzabekov228@gmail.com>
2024-04-25 17:04:32 +0300

    adding user purchased book list

---
+++ client/src/components/PurchasedBookList/index.tsx
@@ -0,0 +1,27 @@
+import { Book } from '@prisma/client'
+import styles from './PurchasedBookList.module.scss'
+import { Link } from 'react-router-dom'
+
+type TPurchasedBookList = {
+    books: Book[]
+}
+export const PurchasedBookList = ({ books }: TPurchasedBookList) => {
+    return (
+        <div className={styles.purchasedBookList}>
+            {books?.map(book =>
+                <div key={book.bookId} className={styles.purchasedBookList__item}>
+                    <div className={styles.purchasedBookList__about}>
+                        <Link to={`/previewBook/${book.bookId}`}>
+                            <img src={book?.picture!} alt="" />
+                        </Link>
+
+                        <h3>{book.bookName}</h3>
+                    </div>
+                    <div className={styles.purchasedBookList__button}>
+                        <button>Читать</button>
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
--- client/src/pages/MyPurchases/index.tsx
+++ client/src/pages/MyPurchases/index.tsx
@@ -1,3 +1,28 @@
+import { useSelector } from "react-redux"
+import { useGetAllPurchasesByUserIdQuery } from "../../app/services/purchase"
+import { selectUser } from "../../features/auth/authSlice"
+import styles from './MyPurchased.module.scss'
+import { Sidebar } from "../../components/Sidebar"
+import { PurchasedBookList } from "../../components/PurchasedBookList"
+import { useGetAllBooksQuery } from "../../app/services/book"
+import { LoadingPage } from "../../components/LoadingPage"
+
 export const MyPurchases = () => {
-    return <div>My Purchases</div>
+    const user = useSelector(selectUser)
+    const purchases = useGetAllPurchasesByUserIdQuery({ userId: Number(user?.userId) })
+
+    const books = useGetAllBooksQuery();
+
+    const purchasedBookList = books.data?.filter(el => purchases.data?.some(pc => el.bookId === pc.bookId));
+
+    if(!purchasedBookList) return <LoadingPage />
+
+    return <div className={styles.myPurchases}>
+        <div>
+            <Sidebar />
+        </div>
+        <div className={styles.myPurchases__list}>
+            <PurchasedBookList books={purchasedBookList}/>
+        </div>
+    </div>
 }
