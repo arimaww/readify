412f366ca78b3887b17eacfc0c1ed15fb50699c0
Islam <islammirzabekov228@gmail.com>
2024-04-25 17:57:42 +0300

    add condition for to check existing book folder

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import {config} from 'dotenv';
+import { config } from 'dotenv';
 import morgan from 'morgan'
 import cors from 'cors'
 config();
@@ -10,16 +10,20 @@ import bookInfo from './Routes/bookInfoRoutes'
 import walletRoutes from './Routes/walletRoutes'
 import purchaseRoutes from './Routes/purchaseRoutes'
 import path from 'path';
+import fs from 'fs'
 
 const app = express();
 const PORT = process.env.PORT!;
 app.use('/static', express.static(path.join(__dirname, "/assets")))
-
+const booksDirectory = path.join(__dirname, './assets/books');
+if (!fs.existsSync(booksDirectory)) {
+    fs.mkdirSync(booksDirectory, { recursive: true });
+}
 
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 
 app.use('/api/user/', userRoutes)
