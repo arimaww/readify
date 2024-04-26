b6910ab65cb05464db7d0afd9429c23bba208112
Islam <islammirzabekov228@gmail.com>
2024-04-26 13:39:11 +0300

    loading page has been added

--- client/src/features/auth/auth.tsx
+++ client/src/features/auth/auth.tsx
@@ -1,9 +1,10 @@
 import { useCurrentQuery } from "../../app/services/auth";
+import { LoadingPage } from "../../components/LoadingPage";
 
 export const Auth = ({children}: {children: JSX.Element}) => {
     const {isLoading} = useCurrentQuery();
 
-    if(isLoading) return <span>Загрузка...</span>
+    if(isLoading) return <LoadingPage />
 
     return children;
 }
