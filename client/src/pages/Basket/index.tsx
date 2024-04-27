7ecfe1d7c6a005412e8817ac84cc34500d31c03d
Islam <islammirzabekov228@gmail.com>
2024-04-27 11:21:56 +0300

    console log and empty space were removed

--- client/src/pages/Basket/index.tsx
+++ client/src/pages/Basket/index.tsx
@@ -25,10 +25,7 @@ export const Basket = () => {
   const basketList = book.data?.filter(book => basket.data?.some(el => book.bookId == el.bookId && el.userId == user?.userId));
   const favorites = useGetUserFavoritesQuery({ userId: user?.userId });
   const entryPrice = basketList?.reduce((val, acc) => val += acc.cost, 0);
-
-  console.log(basketList)
   
-
   const handlePurchaseBook = async (data: Omit<Purchase, "purchasedId">[]) => {
     try {
       data.map(async el => await purchaseBook(el).unwrap())
@@ -47,9 +44,7 @@ export const Basket = () => {
     }
   }
 
-
   return (
-
     <div className={styles.basket}>
       <h2>Моя корзина</h2>
       <div className={styles.basket__basketBookList}>
