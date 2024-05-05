201cbf89d01e01791712efb24eccf5f80439684a
Islam <islammirzabekov228@gmail.com>
2024-05-05 14:16:43 +0300

    (fix) when getting author's books instead all of them

--- client/src/pages/Favorite/index.tsx
+++ client/src/pages/Favorite/index.tsx
@@ -2,13 +2,13 @@ import { useSelector } from "react-redux"
 import { useGetUserFavoritesQuery } from "../../app/services/auth"
 import { selectUser } from "../../features/auth/authSlice"
 import { BookListCards } from "../../components/BookListCards";
-import { useGetBookListQuery } from "../../app/services/book";
+import { useGetAllBooksQuery } from "../../app/services/book";
 import { LoadingPage } from "../../components/LoadingPage";
 
 const Favorite = () => {
   const user = useSelector(selectUser);
   const favorites = useGetUserFavoritesQuery({userId: user?.userId})
-  const { data } = useGetBookListQuery({ id: user?.userId });
+  const { data } = useGetAllBooksQuery();
   const fd = data?.filter(el => favorites.data?.some(fav => fav.bookId === el.bookId));
 
   if(!data || favorites.isLoading)
