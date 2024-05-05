3b7985fded411b3b00e9d4e7cda738b3b47a058f
Islam <islammirzabekov228@gmail.com>
2024-05-05 14:09:34 +0300

    adding sidebar mobile and responsive

--- client/src/pages/MyPurchases/index.tsx
+++ client/src/pages/MyPurchases/index.tsx
@@ -2,27 +2,29 @@ import { useSelector } from "react-redux"
 import { useGetAllPurchasesByUserIdQuery } from "../../app/services/purchase"
 import { selectUser } from "../../features/auth/authSlice"
 import styles from './MyPurchased.module.scss'
-import { Sidebar } from "../../components/Sidebar"
 import { PurchasedBookList } from "../../components/PurchasedBookList"
 import { useGetAllBooksQuery } from "../../app/services/book"
 import { LoadingPage } from "../../components/LoadingPage"
+import { useState } from "react"
+import { SidebarMobile } from "../../components/SidebarMobile"
 
 export const MyPurchases = () => {
     const user = useSelector(selectUser)
     const purchases = useGetAllPurchasesByUserIdQuery({ userId: Number(user?.userId) })
+    const [isOpen, setIsMenuOpen] = useState<boolean>(false);
 
     const books = useGetAllBooksQuery();
 
     const purchasedBookList = books.data?.filter(el => purchases.data?.some(pc => el.bookId === pc.bookId));
 
-    if(!purchasedBookList) return <LoadingPage />
+    if (!purchasedBookList) return <LoadingPage />
 
     return <div className={styles.myPurchases}>
-        <div>
-            <Sidebar />
+        <div className={styles.myPurchases__menu}>
+            <SidebarMobile isOpen={isOpen} setIsMenuOpen={setIsMenuOpen} />
         </div>
         <div className={styles.myPurchases__list}>
-            <PurchasedBookList books={purchasedBookList}/>
+            <PurchasedBookList books={purchasedBookList} />
         </div>
     </div>
 }
